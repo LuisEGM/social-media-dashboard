aa3c6ff1f6f5fe0348dd49d5461020122d3cf1b9
LuisEGM <gamezluis777@gmail.com>
2021-02-16 00:09:02 -0500

    Dark mode terminado

--- js/app.js
+++ js/app.js
@@ -4,11 +4,21 @@ const checkbox = document.querySelector("#checkbox");
 // console.log(window.matchMedia("(prefers-color-scheme: dark)"));
 
 if(window.matchMedia("(prefers-color-scheme: dark)").matches){
-    console.log("que pedo");
     checkbox.setAttribute("checked",true)
 }
 
+
+
 checkbox.addEventListener("change",function () {
     console.log("cambio el toggle");
-    document.body.classList.toggle("is-dark-mode");
+
+    if(this.checked){
+        document.body.classList.add("is-dark-mode");
+        document.body.classList.remove("is-light-mode");
+    }
+    else{
+        document.body.classList.add("is-light-mode");
+        document.body.classList.remove("is-dark-mode");
+    }
+
 })
